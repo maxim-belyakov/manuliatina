a356f85f42a7d199e5674c08fbc8d4cfc51a9430
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-06-17 19:10:38 +0300

    update newStory.js

--- src/story/newStory.js
+++ src/story/newStory.js
@@ -1,26 +1,11 @@
-// bg
-const myRoom = require("./bg/комната гг 3.jpg");
-const hall = require("./bg/корридор гг 3.jpg");
-const street = require("./bg/Лесная День.jpg");
-// bgm
-const field = require("./bgm/Field.mp3");
-
-// speakers
-const b = "Block"; // Представляет строку в пузырьке над текстовым полем. Также заключает текст в кавычки.
-
-// sprites
-// const bn = require("./sprites/block-neutral.png");
-// const bh = require("./sprites/block-happy.png");
-// const bp = require("./sprites/block-pout.png");
-
 let location = {
-    my_room: {
+    myRoom: {
         "_id": "5ed4dcd2dd1b429f7d602212",
         "title": "Моя комната",
-        "original": "комната гг 7.png",
-        "sunrise": "комната гг 5.png",
-        "sunset": "комната гг 5.png",
-        "night": "комната гг 3.png",
+        "original": "комната гг 7.jpg",
+        "sunrise": "комната гг 5.jpg",
+        "sunset": "комната гг 5.jpg",
+        "night": "комната гг 3.jpg",
         "music": [
             {
                 "name": "Field.mp3",
@@ -84,11 +69,11 @@ let location = {
             }
         ]
     },
-    lesnaya_street: {
+    lesnayaStreet: {
         "_id": "5ed4e1a6dd1b429f7d602219",
         "title": "Улица “Лесная”",
-        "original": "Лесная День",
-        "night": "Лесная Ночь",
+        "original": "Лесная День.jpg",
+        "night": "Лесная Ночь.jpg",
         "music": [
             {
                 "name": "morning_birds.mp3",
@@ -109,8 +94,8 @@ let location = {
     hall: {
         "_id": "5ed4e0c6dd1b429f7d602216",
         "title": "Коридор",
-        "original": "корридор гг 3.png",
-        "night": "корридор гг 5.png",
+        "original": "корридор гг 3.jpg",
+        "night": "корридор гг 5.jpg",
         "music": [
             {
                 "name": "Navprostets.mp3",
@@ -154,75 +139,27 @@ let location = {
         ]
     }
 }
-
-let story = [
-  {
-    routeBegins: "myRoom",
-    bg: myRoom,
-  },
-  { 
-    bgm: field,
-    choicesExist: true, 
-    choices: "myRoom"
-  },
+export default location;
 
 
-  // Hall
-  { 
-    routeBegins: "hall", 
-    bg: hall,
-    bgm: field,
-  },
-  { 
-    choicesExist: true, 
-    choices: "hall"
-  },
-
-  // Shower
-  {
-    routeBegins: "shower",
-    jumpTo: "myRoom"
-  },
-
-  // Sleep
-  {
-    routeBegins: "sleep",
-    jumpTo: "myRoom"
-  },
-
-
-  // Storing choices
-  {
-    routeBegins: "showStoringChoices",
-    spriteLeft: "",
-    text: "The user is jumped to a specific index depending on what choice is clicked on."
-  },
-  { 
-    choicesExist: true,
-    text: "Help?",
-    choice: 'test'
-  },
-];
+// Переделать под объекты, имеющие имена
+// А не массив безымянных объектов как сейчас
 
 // The code below is to set undefined properties to the last defined property.
 // It is optional and based on preference, so feel free to add or remove any function calls.
 
-setFutureProperties("bg");
-setFutureProperties("bgm");
-setFutureProperties("speaker");
-setFutureProperties("sprite");
-setFutureProperties("spriteLeft");
-setFutureProperties("spriteRight");
+// setFutureProperties("bg");
+// setFutureProperties("bgm");
+// setFutureProperties("speaker");
+// setFutureProperties("sprite");
+// setFutureProperties("spriteLeft");
+// setFutureProperties("spriteRight");
 
-function setFutureProperties(key) {
-  let cache = "";
-  for (let obj of story) {
-    if (obj[key] || obj[key] === "") cache = obj[key];
-    else obj[key] = cache;
-
-    // Переделать под объекты, имеющие имена
-    // А не массив безымянных объектов как сейчас
-  }
-}
-
-export default location;
+// function setFutureProperties(key) {
+//   let cache = "";
+//   for (let obj of story) {
+//     if (obj[key] || obj[key] === "") cache = obj[key];
+//     else obj[key] = cache;
+    
+//   }
+// }
